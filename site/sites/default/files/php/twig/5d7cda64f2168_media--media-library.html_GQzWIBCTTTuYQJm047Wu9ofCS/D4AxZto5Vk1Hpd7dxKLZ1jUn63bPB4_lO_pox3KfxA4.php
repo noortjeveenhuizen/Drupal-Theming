d1301d2dd10f42dbd730dd08910b4d253142826e
Pieter Mathys <mathysp@gmail.com>
2019-09-14 14:23:52 +0200

    Init

---
+++ site/sites/default/files/php/twig/5d7cda64f2168_media--media-library.html_GQzWIBCTTTuYQJm047Wu9ofCS/D4AxZto5Vk1Hpd7dxKLZ1jUn63bPB4_lO_pox3KfxA4.php
@@ -0,0 +1,126 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/modules/media_library/templates/media--media-library.html.twig */
+class __TwigTemplate_6938cd9afb87fee8693b0687f1d58611d596ef4aa9d58e6113568fe82c8d25a1 extends \Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $tags = ["if" => 37];
+        $filters = ["escape" => 36, "without" => 39, "t" => 42];
+        $functions = [];
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['escape', 'without', 't'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 36
+        echo "<article";
+        echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["attributes"] ?? null)), "html", null, true);
+        echo ">
+  ";
+        // line 37
+        if (($context["content"] ?? null)) {
+            // line 38
+            echo "    <div";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["preview_attributes"] ?? null)), "html", null, true);
+            echo ">
+      ";
+            // line 39
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->env->getExtension('Drupal\Core\Template\TwigExtension')->withoutFilter($this->sandbox->ensureToStringAllowed(($context["content"] ?? null)), "name"), "html", null, true);
+            echo "
+    </div>
+    ";
+            // line 41
+            if ( !($context["status"] ?? null)) {
+                // line 42
+                echo "      <div class=\"media-library-item__status\">";
+                echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->renderVar(t("unpublished"));
+                echo "</div>
+    ";
+            }
+            // line 44
+            echo "    <div";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["metadata_attributes"] ?? null)), "html", null, true);
+            echo ">
+      <div class=\"media-library-item__name\">
+        <a href=\"";
+            // line 46
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["url"] ?? null)), "html", null, true);
+            echo "\" rel=\"bookmark\">";
+            echo $this->env->getExtension('Drupal\Core\Template\TwigExtension')->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["name"] ?? null)), "html", null, true);
+            echo "</a>
+      </div>
+    </div>
+  ";
+        }
+        // line 50
+        echo "</article>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/modules/media_library/templates/media--media-library.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  95 => 50,  86 => 46,  80 => 44,  74 => 42,  72 => 41,  67 => 39,  62 => 38,  60 => 37,  55 => 36,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/modules/media_library/templates/media--media-library.html.twig", "/Users/pietermathys/Projects/IMD-THEMING/site/core/modules/media_library/templates/media--media-library.html.twig");
+    }
+}
